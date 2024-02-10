cd6acd688c9cbfb0f83736dbd993b2a405aa14a2
DatoQoroglishvili <koroglishvilidato@gmail.com>
2024-02-09 23:27:03 -0800

    commit

--- day3/classwork.py
+++
@@ -1,8 +0,0 @@
-fr om tu rtle i mport*
-print(dato data)
-print"hello world"
-name+Ssk=data + =ko ro glob
-prin t+ f orwa r d =inedzh
-printr+++ incredibly=chaa d
-print(Divn dvd do do Cor)
-print(++ da=)

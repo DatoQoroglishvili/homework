22c61a23155fcc13f7b694e8791a325a0f106823
DatoQoroglishvili <koroglishvilidato@gmail.com>
2024-02-13 09:18:50 -0800

    commit

---
+++ extra projects/1.py
@@ -0,0 +1,7 @@
+running = True
+
+while running:
+    number = int(input("enter kilogramm"))
+    for multyplay in range(1) :
+        print(number,"*",2.204,"=",number*2.204)
+#when you click run,it will show;enter kilogramm, and you have to write your kilogramm and it will calculate and show your kilogramms in LBS.

2431746e8014c3635c857463aaf4d3a679e04e16
DatoQoroglishvili <koroglishvilidato@gmail.com>
2024-02-23 08:53:22 -0800

    commit

---
+++ day 07/classwork/classwork2.py
@@ -0,0 +1,10 @@
+
+num1 = int(input("Please enter your number 1: "))
+num2 = int(input("Please enter your number 2: "))
+
+print(num1 < num2)
+print(num1 > num2)
+print(num1 == num2)
+
+
+ 
---
+++ day 07/classwork/classwork3.py
@@ -0,0 +1,12 @@
+print(int(69))
+print(int("6") + 10)
+
+print(float("6.9"))
+print(float(8))
+
+print(str(10) + "15")
+print(str(6.4) + "10")
+
+print(type(99.1))
+print(type(10))
+print(type("dato"))

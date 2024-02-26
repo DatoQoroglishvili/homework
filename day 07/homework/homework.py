459a741a443f24c7438c94afb4aec193bfdf1c26
DatoQoroglishvili <koroglishvilidato@gmail.com>
2024-02-26 07:44:02 -0800

    commit

--- day 07/homework/homework.py
+++ day 07/homework/homework.py
@@ -1,12 +1,13 @@
+#int is number for examples(5,3,2,1,4,6,7,8)
 print(int(10.0))
 print(int("6") + 10)
-
+#float is (9.6,6.5 and 3,1)
 print(float("6.9"))
 print(float(8.0))
-
+#str is when it has ""
 print(str(10) + "15")
 print(str(690.4) + "10")
-
+#type tell us what variables is in terminal
 print(type(9.2))
 print(type(190))
 print(type("dato"))

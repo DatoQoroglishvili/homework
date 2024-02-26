0678536a94e786cc18934061aa33438c902dc843
DatoQoroglishvili <koroglishvilidato@gmail.com>
2024-02-26 07:40:53 -0800

    commit

---
+++ day 07/homework/homework.py
@@ -0,0 +1,12 @@
+print(int(10.0))
+print(int("6") + 10)
+
+print(float("6.9"))
+print(float(8.0))
+
+print(str(10) + "15")
+print(str(690.4) + "10")
+
+print(type(9.2))
+print(type(190))
+print(type("dato"))

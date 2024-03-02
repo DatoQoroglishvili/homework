1ceda1f21ce3dd0270eddd94d4728c17d58136a0
DatoQoroglishvili <koroglishvilidato@gmail.com>
2024-03-02 11:44:17 -0800

    commit

---
+++ day 08/homework/homework.py
@@ -0,0 +1,62 @@
+user_input=int(input("please enter number"))
+
+if user_input==9:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==8:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==10:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==41:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==55:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==556:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==107:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==-9:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==985:
+    print("you won!")
+else:
+    print("you lose!")

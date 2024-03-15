b3bb510d2da71485c5916fc3ffae63d2ce2ad50e
DatoQoroglishvili <koroglishvilidato@gmail.com>
2024-03-15 10:04:46 -0700

    commit

---
+++ day 12/homework/homework.py
@@ -0,0 +1,65 @@
+#1
+toy=20
+budget=int(input("please enter your budget :"))
+if budget>=toy:
+    print("you can buy the toy")
+else:
+    print("you cant buy the toy")
+
+#2
+registered_password = "dato2009"
+password = input("Please enter your password: ")
+
+while password != registered_password:
+    password = input("Please enter your password again: ")
+    if password == registered_password:
+        print("You have accses on your account")
+    else:
+        print("You have entered wrong password, please try again")
+
+#3
+start = int(input("Please enter starting value: "))
+end = int(input("Please enter ending value: "))
+step = int(input("Please enter step value: "))
+
+for i in range(start,end,step):
+    print(i)
+#4
+s1 = int(input("Please enter first side: "))
+s2 = int(input("Please enter second side: "))
+s3 = int(input("Please enter third side: "))
+
+
+is_valid = (s1 + s2 > s3) and (s2 + s3 > s1) and (s3 + s1 > s2)
+
+while is_valid != True:
+    s1 = int(input("Please enter first side: "))
+    s2 = int(input("Please enter second side: "))
+    s3 = int(input("Please enter third side: "))
+
+    is_valid = s1 + s2 > s3
+
+#5?????
+    
+
+
+
+
+
+#6
+operation = input("Please enter operation (+,-,*,/): ")
+num1 = int(input("Please enter first number: "))
+num2 = int(input("Please enter second number: "))
+
+if operation == '+':
+    print(num1 + num2)
+elif operation == '-':
+    print(num1 - num2)
+elif operation == '*':
+    print(num1 * num2)
+elif operation == '/':
+    print(num1 / num2)
+else:
+    print("operation is not valid")
+
+#7
---
+++ day 13/classwork/classwork2.py
@@ -0,0 +1,9 @@
+num = int(input("your age: "))
+
+if num > 0 and num < 18:
+    print("you are child")
+elif num > 18 and num <51:
+    print("you are adult")
+else:
+    print("you are old ")
+

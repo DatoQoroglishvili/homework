bb59db1b2ef4d9c842f5cefcf23ef288ea7cad2d
DatoQoroglishvili <koroglishvilidato@gmail.com>
2024-03-18 06:27:51 -0700

    commit

---
+++ extra projects/3.py
@@ -0,0 +1,35 @@
+num=input("please enter yours idea who is best player in nba history:")
+if num=="lebron james ":
+    print("you are wrong because he is second best player ")
+elif num=="michael jordan":
+    print("you are right!!!")
+elif num=="michael jekson":
+    print("you are god damn right, HE HE")
+elif num=="Stef curry":
+    print("not reallyy but best shooter in nba")
+elif num=="luka doncic":
+    print("you are freak")
+elif num=="ja morant":
+    print("not really but best dunker RAHHHHHHH")
+elif num=="dennis rodmen":
+    print("idk best player but he is best rebounder")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+else:
+    print("idk what are you thinking about not going to lie")

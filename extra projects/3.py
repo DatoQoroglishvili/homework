3fc8d3effe0b2508e5ce72837b30bc14a0373fcd
DatoQoroglishvili <koroglishvilidato@gmail.com>
2024-03-18 12:04:20 -0700

    commit

--- extra projects/3.py
+++ extra projects/3.py
@@ -1,35 +1,50 @@
 num=input("please enter yours idea who is best player in nba history:")
+#currently active players
 if num=="lebron james ":
     print("you are wrong because he is second best player ")
-elif num=="michael jordan":
-    print("you are right!!!")
-elif num=="michael jekson":
-    print("you are god damn right, HE HE")
-elif num=="Stef curry":
-    print("not reallyy but best shooter in nba")
+elif num=="Steph curry ":
+    print("not really but best shooter in nba")
 elif num=="luka doncic":
     print("you are freak")
 elif num=="ja morant":
     print("not really but best dunker RAHHHHHHH")
-elif num=="dennis rodmen":
+elif num=="jimmy butler":
+    print("he is michael jordans son so he playes really great!")
+elif num=="giannis antetokounpo":
+    print("he is monster!! not any more words")
+elif num=="victor wembanyama":
+    print("he is so tall but still gent dunked lost of times already :DDD")
+elif num=="sandro mamukelashvili":
+    print("he is georgian so i guess he playes wery well")
+
+
+#end of currently active players
+#lmao
+elif num=="lionel messi":
+    print("wrong greatest of the time")
+elif num=="cristiano ronaldo":
+    print("wrong greatest of the time")
+elif num=="michael jekson":
+    print("you are god damn right, HE HE")
+#end of lmao
+#legends now
+elif num=="ben wallec":
+    print("he was shaq oneal nightmear")
+elif num=="shaq oneal":
+    print("he was one of the best big man ")
+elif num=="antoni davis":
+    print("idk man he kanda have mazda eyebrows")
+elif num=="larry bird":
+    print("he was one of the best trash talker")
+elif num=="michael jordan":
+    print("you are right!!!")
+elif num=="magic jonson":
+    print("he wasone of best passer to")
+elif num=="scottie pipen":
+    print("him and michael jordan was best duo with 6 rings")
+elif num=="will simth":
+    print("there are some information about him that he scored 100 points in 1 game but ist true or not we dont know")
+elif num=="dennis rodmen" :
     print("idk best player but he is best rebounder")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 else:
-    print("idk what are you thinking about not going to lie")
+    print("idk what are you thinking about not going to lie but tell me and i will add to the list :DDDDD")

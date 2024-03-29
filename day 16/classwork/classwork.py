04d665ed34a5b96097aa8082656932e9cabe60f4
DatoQoroglishvili <koroglishvilidato@gmail.com>
2024-03-29 08:35:10 -0700

    commit

---
+++ day 16/classwork/classwork.py
@@ -0,0 +1,13 @@
+list1=["breaking bad","better call soul",]
+list2=["porsche","bmw","ferrari"]
+list3=["basketball","football","rugby"]
+print(list1[1],list2[0],list3[2])
+
+list=["dato","qoroglishvili"]
+print(list[1],list[0])
+
+
+txt=int(input("please enter your age"))
+email=input("please enter you e-mail")
+list=[email,txt]
+print(list)
---
+++ day 16/homework/homework.py
@@ -0,0 +1,32 @@
+#1
+list1=["dato","lizi","vefxo","iza"]
+print(list1[0],list1[1],list1[2],list1[3])
+
+#2
+list2=[1,2,3,4,5,6,7,8,9,10]
+print(list2[0],list2[9])
+
+# #3
+list3=[10,11,12,13,14,15,16,17,18,19,20]
+list3[0] = 1
+list3[2] = 1
+list3[4] = 1
+list3[6] = 1
+list3[8] = 1
+list3[10] = 1
+
+print(list3)
+
+#4
+name=input("please enter your name;")
+surname=input("please enter your surname;")
+age=int(input("please enter your age;"))
+home=input("please enter where you live;")
+list4={name,surname,age,home}
+print(list4)
+
+#5
+list5=["q","o","r","o","g","l","i","s","h","v","i","l","I"]
+print(list5[0],list5[1],list5[2],list5[3],list5[4],list5[5],list5[6],list5[7],list5[8],list5[9],list5[10],list5[11],list5[12])
+
+

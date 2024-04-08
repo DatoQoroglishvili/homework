59f64146015948b095caeb5464120bfe0c5bb806
DatoQoroglishvili <koroglishvilidato@gmail.com>
2024-04-08 04:47:10 -0700

    commit

--- day19/homework/homework.py
+++ day19/homework/homework.py
@@ -1,14 +1,14 @@
 list_names=["dato", "nika", "luka", "lizi","mari"]
 input1=int(input("enter number from 0 to 4;"))
 if input1 == 0:
-    print("dato")
+    print(list_names[0])
 elif input1 == 1:
-    print("nika")
+    print(list_names[1])
 elif input1 == 2:
-    print("luka")
+    print(list_names[2])
 elif input1 == 3:
-    print("lizi")
+    print(list_names[3])
 elif input1 == 4:
-    print("mari")
+    print(list_names[4])
 else:
     print("please try again.")

e3d63ee60eca2bd153f572c21d1152dcacf52c9e
DatoQoroglishvili <koroglishvilidato@gmail.com>
2024-04-20 02:00:15 -0700

    commit

---
+++ day22/classwork/classwork2.py
@@ -0,0 +1,27 @@
+#1
+list=[]
+
+end=20
+start=0
+step=1.5
+
+while start < end:
+    list.append(start)
+    start=start+step
+print(list)
+#2
+names=[1,3,6,7,9,22,24]
+list1=[]
+start1=0
+end1=20
+step1=2
+
+while start1<end1:
+    list1.append(start1)
+    start1=start1+step1
+    print(list1)
+
+
+#3
+name="datuna"
+print(name[3:])
---
+++ day22/classwork/classwork3.py
@@ -0,0 +1,11 @@
+#1
+nums=[]
+
+end=1
+start=20
+step=-2.5
+
+while start > end:
+    nums.append(start)
+    start=start+step
+print(nums)
